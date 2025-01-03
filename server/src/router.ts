16628ff4aec8bf60adb1a472db7b83c01cfd9172
Geoffrey <geoffreyleglise@gmail.com>
2025-01-03 12:31:41 +0100

    add router for manager

--- server/src/router.ts
+++ server/src/router.ts
@@ -13,6 +13,13 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
+// Define item-related routes
+import managerActions from "./modules/manager/managerActions";
+
+router.get("/api/managers", itemActions.browse);
+router.get("/api/managers/:id", itemActions.read);
+router.post("/api/managers", itemActions.add);
+
 /* ************************************************************************* */
 
 export default router;

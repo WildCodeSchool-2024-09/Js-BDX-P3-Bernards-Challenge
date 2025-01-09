3d9182e417734404069cf35c6587d91138377670
Geoffrey <geoffreyleglise@gmail.com>
2025-01-09 10:28:35 +0100

    add ON DELETE CASCADE

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS Bernards_Challenge ;
 
 use Bernards_Challenge;
-CREATE TABLE user (
+CREATE TABLE  user (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -16,14 +16,14 @@ CREATE TABLE application_user (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT ,
     password VARCHAR(60) NOT NULL,
     user_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
 CREATE TABLE administators (
   id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT ,
   application_user_id INT UNSIGNED NOT NULL,
  FOREIGN KEY (application_user_id) REFERENCES
-  application_user(id)
+  application_user(id) ON DELETE CASCADE
 );
 
 -- Table: enterprise
@@ -38,7 +38,7 @@ CREATE TABLE manager (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     application_user_id INT UNSIGNED NOT NULL,
     enterprise_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (application_user_id) REFERENCES application_user(id),
+    FOREIGN KEY (application_user_id) REFERENCES application_user(id) ON DELETE CASCADE ,
     FOREIGN KEY (enterprise_id) REFERENCES enterprise(id)
 );
 
@@ -48,7 +48,7 @@ CREATE TABLE channel_slack (
     channel_name VARCHAR(255) NOT NULL,
     channel_id VARCHAR(255) NOT NULL,
     entreprise_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (entreprise_id) REFERENCES enterprise(id)
+    FOREIGN KEY (entreprise_id) REFERENCES enterprise(id) ON DELETE CASCADE
 );
 
 -- Table: question
@@ -71,7 +71,7 @@ CREATE TABLE user_player (
     response_id INT UNSIGNED,
     user_id INT UNSIGNED,
     FOREIGN KEY (response_id) REFERENCES response(id),
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
 -- Table: channel_slack_user_player
@@ -94,14 +94,14 @@ CREATE TABLE team_player (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     quizz_id INT UNSIGNED,
-    FOREIGN KEY (quizz_id) REFERENCES quizz(id)
+    FOREIGN KEY (quizz_id) REFERENCES quizz(id) ON DELETE CASCADE
 );
 
 -- Table: user_player_team_player
 CREATE TABLE user_player_team_player (
     user_player_id INT UNSIGNED NOT NULL,
     team_player_id INT UNSIGNED NOT NULL,
-    FOREIGN KEY (user_player_id) REFERENCES user_player(id),
+    FOREIGN KEY (user_player_id) REFERENCES user_player(id) ON DELETE CASCADE,
     FOREIGN KEY (team_player_id) REFERENCES team_player(id)
 );
 
@@ -110,7 +110,7 @@ CREATE TABLE quizz_question (
     quizz_id INT UNSIGNED NOT NULL,
     question_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (quizz_id) REFERENCES quizz(id),
-    FOREIGN KEY (question_id) REFERENCES question(id)
+    FOREIGN KEY (question_id) REFERENCES question(id) ON DELETE CASCADE
 );
 
 -- Table: channel_slack_quizz
@@ -118,7 +118,7 @@ CREATE TABLE channel_slack_quizz (
     channel_slack_id INT UNSIGNED NOT NULL,
     quizz_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (channel_slack_id) REFERENCES channel_slack(id),
-    FOREIGN KEY (quizz_id) REFERENCES quizz(id)
+    FOREIGN KEY (quizz_id) REFERENCES quizz(id) ON DELETE CASCADE
 );
 
 -- Table: boolean_responses

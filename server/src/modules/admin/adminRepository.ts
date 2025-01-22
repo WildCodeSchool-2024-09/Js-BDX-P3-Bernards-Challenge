4096ffe3247d2aee7cf9e9274094f1e01e1e0d45
Raphaël <barre.raphael97@gmail.com>
2025-01-22 15:51:18 +0100

    Update admin

--- server/src/modules/admin/adminRepository.ts
+++ server/src/modules/admin/adminRepository.ts
@@ -22,30 +22,30 @@ class AdminRepository {
 
       const [userResult] = await connection.query<Result>(
         "INSERT INTO user (email, first_name, last_name) VALUES (?, ?, ?)",
-        [adminData.email, adminData.first_name, adminData.last_name]
+        [adminData.email, adminData.first_name, adminData.last_name],
       );
 
       const userId = userResult.insertId;
-      if(!userId){
-        throw new Error ("Failed insert into user")
+      if (!userId) {
+        throw new Error("Failed insert into user");
       }
 
       const [appUserResult] = await connection.query<Result>(
         "INSERT INTO application_user (password, user_id) VALUES (?, ?)",
-        [adminData.password, userId]
+        [adminData.password, userId],
       );
 
       const applicationUserId = appUserResult.insertId;
-      if(!applicationUserId){
-        throw new Error ("Failed insert into application_user")
+      if (!applicationUserId) {
+        throw new Error("Failed insert into application_user");
       }
 
       const [adminResult] = await connection.query<Result>(
         "INSERT INTO administrators (application_user_id) VALUES (?)",
-        [applicationUserId]
+        [applicationUserId],
       );
-      if (!adminResult.insertId){
-        throw new Error ("Failed insert into administrators")
+      if (!adminResult.insertId) {
+        throw new Error("Failed insert into administrators");
       }
 
       await connection.commit();
@@ -65,7 +65,7 @@ class AdminRepository {
        INNER JOIN application_user au ON a.application_user_id = au.id
        INNER JOIN user u ON au.user_id = u.id
        WHERE a.id = ?`,
-      [id]
+      [id],
     );
     return rows[0] as User;
   }
@@ -75,7 +75,7 @@ class AdminRepository {
       `SELECT u.email, u.first_name, u.last_name
        FROM administrators a
        INNER JOIN application_user au ON a.application_user_id = au.id
-       INNER JOIN user u ON au.user_id = u.id`
+       INNER JOIN user u ON au.user_id = u.id`,
     );
     return rows as User[];
   }
@@ -85,48 +85,45 @@ class AdminRepository {
     try {
       await connection.beginTransaction();
 
-      await connection.query(
+      const [result] = await connection.execute<Result>(
         `UPDATE user u
         INNER JOIN application_user au ON u.id = au.user_id
         INNER JOIN administrators a ON a.application_user_id = au.id
         SET u.email = ?, u.first_name = ?, u.last_name, au.password = ?
         WHERE a.id = ?`,
-        [admin.email, admin.first_name, admin.last_name, admin.password, admin.id]
+        [
+          admin.email,
+          admin.first_name,
+          admin.last_name,
+          admin.password,
+          admin.id,
+        ],
       );
+
+      if (result.affectedRows !== 2) {
+        throw new Error("Failed to update admin");
+      }
 
       await connection.commit();
       return true;
     } catch (err) {
       await connection.rollback();
-      return false;
+      throw err;
     } finally {
       connection.release();
     }
   }
 
   async delete(id: number) {
-    const connection = await databaseClient.getConnection();
-    try {
-      await connection.beginTransaction();
-
-      await connection.query(
-        `DELETE FROM user WHERE id = (
+    const [result] = await databaseClient.execute<Result>(
+      `DELETE FROM user WHERE id = (
           SELECT u.id FROM user u
           INNER JOIN application_user au ON u.id = au.user_id
           INNER JOIN administrators a ON au.id = a.application_user_id
           WHERE a.id = ?)`,
-        [id]
-      );
-
-      await connection.commit();
-      return true;
-    } catch (err) {
-      await connection.rollback();
-      return false;
-    } finally {
-      connection.release();
-    }
+      [id],
+    );
+    return result.affectedRows;
   }
 }
-
 export default new AdminRepository();

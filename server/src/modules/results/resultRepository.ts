c808d2c12f98283cb4e4299db3aee65639ca1761
Geoffrey <geoffreyleglise@gmail.com>
2025-01-30 12:13:39 +0100

    switch branch login for stash

--- server/src/modules/results/resultRepository.ts
+++ server/src/modules/results/resultRepository.ts
@@ -1,6 +1,6 @@
 import databaseClient from "../../../database/client";
 
-import type { Result, Rows } from "../../../database/client";
+import type { Rows } from "../../../database/client";
 
 class resultRepository {
   async read(id: number) {
